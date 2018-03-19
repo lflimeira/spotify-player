0a3cbd209fc730623be9fc95779c72a5ad3eae19
Luiz Felipe <lflimeira02@gmail.com>
2018-03-19 00:04:28 -0300

    Creates AlbumList component
    Creates AlbumList component and adds it in the player.

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotfyWrapper from 'sdk-spotify'
 
 const spotify = new SpotfyWrapper({
-  token: ''
+  token: 'BQAlcR7TTjmgXA2rnhdShLmM0VPh7oh_Fp0mnQMecwpAu9NBqYnt56cTp1j5QUnF3aftYvvlMY1Kn9EWbJ1fYMNG9mrC9RmHtHRm95WVK3-8CDBCsDDfIuSAvn9NK6Rmg1XURUSYDQGzgr2O'
 })
 
 export default spotify
