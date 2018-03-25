4b23f5d08ac5053de93ab9dfaacf89a7d2f51fcc
Luiz Felipe <lflimeira02@gmail.com>
2018-03-24 21:32:34 -0300

    Show album info in the player

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import spotify from './Spotify'
 import renderAlbums from './AlbumList'
+import renderAlbumInfo from './AlbumInfo'
 
 const albums = spotify.search.searchAlbums('Incubus')
 const albumList = document.getElementById('album-list')
 
+const album = spotify.album.getAlbum('0sNOF9WDwhWunNAHPD3Baj')
+const albumInfo = document.getElementById('album-info')
+
 albums
   .then(data => renderAlbums(data.albums.items, albumList))
+
+album
+  .then(data => renderAlbumInfo(data, albumInfo))
