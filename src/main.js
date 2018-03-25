668b6429a630a90e36cb784844bf43c753ed99f6
Luiz Felipe <lflimeira02@gmail.com>
2018-03-25 00:28:03 -0300

    Creates trigger to the search event in the player

--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
 import spotify from './Spotify'
-import renderAlbums from './AlbumList'
 import renderAlbumInfo from './AlbumInfo'
 import renderAlbumTracks from './AlbumTracks'
+import searchEnterTrigger from './SearchTrigger'
 
-const albums = spotify.search.searchAlbums('Incubus')
-const albumList = document.getElementById('album-list')
+searchEnterTrigger()
 
 const album = spotify.album.getAlbum('0sNOF9WDwhWunNAHPD3Baj')
 const albumInfo = document.getElementById('album-info')
 const trackList = document.getElementById('album-tracks')
-
-albums
-  .then(data => renderAlbums(data.albums.items, albumList))
 
 album
   .then(data => renderAlbumInfo(data, albumInfo))
