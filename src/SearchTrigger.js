295f253cac7102f87723e1be7b4b3ba6aa0bc3ec
Luiz Felipe <lflimeira02@gmail.com>
2018-03-26 23:31:55 -0300

    Creates the select album trigger

--- src/SearchTrigger.js
+++ src/SearchTrigger.js
@@ -5,10 +5,14 @@ const albumList = document.getElementById('album-list')
 const searchInput = document.getElementById('search-input')
 const searchForm = document.getElementById('search-form')
 
+function makeRequest() {
+  spotify.search.searchAlbums(searchInput.value)
+    .then(data => renderAlbums(data.albums.items, albumList));
+}
+
 export default function searchEnterTrigger() {
   searchForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    spotify.search.searchAlbums(searchInput.value)
-      .then(data => renderAlbums(data.albums.items, albumList))
-  })
+    e.preventDefault();
+    makeRequest();
+  });
 }
